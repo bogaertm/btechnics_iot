f4a0e455fa4559136df98b1995f271f8aa518429
bogaertm <37183577+bogaertm@users.noreply.github.com>
2026-04-02 15:36:05 +0200

    v1.3.0: voeg tekstgrootte toe

--- custom_components/btechnics_branding/config_flow.py
+++ custom_components/btechnics_branding/config_flow.py
@@ -6,6 +6,8 @@ from homeassistant.core import callback
 DOMAIN = "btechnics_branding"
 DEFAULT_LOGIN_TEXT = "Btechnics IOT"
 DEFAULT_SIDEBAR_TEXT = "Btechnics IOT"
+DEFAULT_LOGIN_SIZE = 24
+DEFAULT_SIDEBAR_SIZE = 16
 
 
 class BtechnicsBrandingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -32,7 +34,7 @@ class BtechnicsBrandingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 
 class BtechnicsBrandingOptionsFlow(config_entries.OptionsFlow):
-    """Opties flow voor teksten instellen."""
+    """Opties flow voor teksten en groottes instellen."""
 
     async def async_step_init(self, user_input=None):
         """Toon instellingen."""
@@ -43,13 +45,13 @@ class BtechnicsBrandingOptionsFlow(config_entries.OptionsFlow):
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Optional(
-                    "login_text",
-                    default=current.get("login_text", DEFAULT_LOGIN_TEXT),
-                ): str,
-                vol.Optional(
-                    "sidebar_text",
-                    default=current.get("sidebar_text", DEFAULT_SIDEBAR_TEXT),
-                ): str,
+                vol.Optional("login_text",
+                    default=current.get("login_text", DEFAULT_LOGIN_TEXT)): str,
+                vol.Optional("login_text_size",
+                    default=current.get("login_text_size", DEFAULT_LOGIN_SIZE)): int,
+                vol.Optional("sidebar_text",
+                    default=current.get("sidebar_text", DEFAULT_SIDEBAR_TEXT)): str,
+                vol.Optional("sidebar_text_size",
+                    default=current.get("sidebar_text_size", DEFAULT_SIDEBAR_SIZE)): int,
             }),
         )
